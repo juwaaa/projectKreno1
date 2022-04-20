f93b4577a66bad9e87ea6b37fa321a8fa18e4a23
juwaaa <najwaalya14@gmail.com>
2022-04-21 00:08:23 +0800

    make it better

--- zizozi/settings.py
+++ zizozi/settings.py
@@ -128,4 +128,5 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
 
